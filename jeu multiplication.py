374fc4d7d35e1bdfc23f7b60255f5b917e13261d
Arthur Correnson <arthur.correnson@gmail.com>
2018-09-06 23:02:06 +0200

    fix small bugs

--- jeu multiplication.py
+++ jeu multiplication.py
@@ -1,20 +1,41 @@
+'''
+  Un jeu ou il faut deviner le plus vite possible 
+  les tables de multiplications
+'''
+
 import os
-'''un jeu ou il faut deviner le plus vite possible les tables de multiplications'''
 from random import randrange
 
-premierNombre=randrange(10)
-deuxiemeNombre=randrange(10)
+premierNombre = randrange(10)
+deuxiemeNombre = randrange(10)
 
-premierNombre=int(premierNombre)
-deuxiemeNombre=int(deuxiemeNombre)
+premierNombre = int(premierNombre)
+deuxiemeNombre = int(deuxiemeNombre)
 
-score=premierNombre*deuxiemeNombre
+score = premierNombre * deuxiemeNombre
 print(premierNombre, "*" , deuxiemeNombre)
-input(int(essai))
-if essai==score:
-    print("gg")
-    end
+
+essai = input(">")
+
+if int(essai) == score:
+    print("bravo !")
 else:
-    print("non")
-    end
-os.system("pause")
+    print("erreur !")
+
+# l'utilisation de commande "pause" n'est
+# pas recommandée car elle n'est pas
+# disponible sur tous les OS
+# os.system("pause")
+
+# on utilise pluttôt un simple "input"
+# pour attendre une instruction avant
+# la fin de l'execution du script
+input('- fin du jeu -')
+
+
+
+
+
+
+
+
